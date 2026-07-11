9e495eb5ab802f5f9d4fd0c7ac40853805a40954
annu-adi <rashmi.maurya10@gmail.com>
2026-07-12 00:18:50 +0530

    Update API endpoints to use Vercel deployment

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -35,7 +35,7 @@ const AppointmentForm = () => {
   useEffect(() => {
     const fetchDoctors = async () => {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/user/doctors",
+        "https://vercel-backeee.vercel.app/api/v1/user/doctors",
         { withCredentials: true }
       );
       setDoctors(data.doctors);
@@ -47,7 +47,7 @@ const AppointmentForm = () => {
     e.preventDefault();
     try {
       const hasVisitedBool = Boolean(hasVisited);
-      const { data } = await axios.post("http://localhost:4000/api/v1/appointment/post", {
+      const { data } = await axios.post("https://vercel-backeee.vercel.app/api/v1/appointment/post", {
         firstName,
 
         lastName,
